1606f824279746054652a74d19518a1a8c3a6e32
Lenovo <anilkumareddy9757@gmail.com>
2024-02-26 17:26:20 +0530

    redis conf

--- mysql.sh
+++ mysql.sh
@@ -2,5 +2,6 @@ cp /home/centos/akr-shell/mysql.repo /etc/yum.repos.d/mongo.repo
 dnf module disable mysql -y
 dnf install mysql-community-server -y
 mysql_secure_installation --set-root-pass RoboShop@1
+mysql -uroot -pRoboShop@1
 systemctl enable mysqld
 systemctl start mysqld
