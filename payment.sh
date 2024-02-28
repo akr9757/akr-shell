f42f6c485bcdf86d67910c8e626c49b05d9bcdca
Lenovo <anilkumareddy9757@gmail.com>
2024-02-28 16:56:59 +0530

    Headings

--- payment.sh
+++ payment.sh
@@ -15,7 +15,7 @@ echo -e "\e[32m>>>>>>>>>>> Add Application User <<<<<<<<<<<<<\e[0m"
 useradd ${app_user}
 
 echo -e "\e[32m>>>>>>>>>>> Copy Payment Service <<<<<<<<<<<<<\e[0m"
-sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" ${script_path}/dispatch.service
+sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" ${script_path}/payment.service
 cp ${script_path}/payment.service /etc/systemd/system/payment.service
 
 echo -e "\e[32m>>>>>>>>>>> Add Application Directory <<<<<<<<<<<<<\e[0m"
