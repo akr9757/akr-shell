319890548fefad170f68a0a91c92d615e741c844
Lenovo <anilkumareddy9757@gmail.com>
2024-03-01 14:38:44 +0530

    main code

--- common.sh
+++ common.sh
@@ -1,4 +1,7 @@
 app_user=roboshop
+script=$(realpath "$0")
+script_path=$(dirname "${script}")
+source ${script_path}/common.sh
 
 func_print_head() {
   echo -e "\e[32m>>>>>>>>>>> $1 <<<<<<<<<<<<<\e[0m"
