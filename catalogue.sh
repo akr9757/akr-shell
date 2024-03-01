38ba54e465462ae83897ad809a676daf6833b509
Lenovo <anilkumareddy9757@gmail.com>
2024-03-01 15:05:08 +0530

    main code

--- catalogue.sh
+++ catalogue.sh
@@ -2,11 +2,11 @@ script=$(realpath "$0")
 script_path=$(dirname "${script}")
 source ${script_path}/common.sh
 
-component="catalogue"
+component=catalogue
 
 func_nodejs
 
-schema_setup="mongo"
+schema_setup=mongo
 
 
 
